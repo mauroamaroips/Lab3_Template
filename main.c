598cbdfe2abbe39c4388c14b18209b6cd9209ce8
Bruno Silva <brunomnsilva@gmail.com>
2023-02-10 17:56:08 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include "input.h"
+
+int main() {
+
+    int seq[50] = {
+            93, 32, 31, 36, 60, 1, 74, 87, 52, 2,
+            76, 54, 74, 17, 90, 81, 96, 72, 22, 31,
+            49, 83, 99, 1, 11, 55, 86, 39, 6, 10,
+            48, 65, 52, 10, 86, 82, 43, 4, 3, 6,
+            78, 35, 31, 82, 18, 1, 48, 80, 23, 38
+            };
+   
+    /* Program code. */
+    printf("I do nothing at this moment.\n");
+
+    return EXIT_SUCCESS;
+}
